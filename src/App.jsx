57cbd16d205f76eb22f4dec9ae290b8bcc9ad23e
Herin Gandhi <142152236+Herin98@users.noreply.github.com>
2024-08-04 00:21:42 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Nav from './components/Nav.jsx'
+import Content from './components/Content.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Nav />
+      <Content />
+    </div>
+  );
+};
+
+export default App
